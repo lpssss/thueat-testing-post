d635c128e2a07365b62317cdb1414796fccb679f
lpssss <lim55570@gmail.com>
2021-12-01 18:57:15 +0800

    Update

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route(' /private/mystall/dishes', methods=['POST'])
+@app.route('/private/mystall/dishes', methods=['POST'])
 def mystall_dishes():
     obj={"foo":"bar", "foo1":"bar1"}
     return Response(json.dumps(obj), status=200, mimetype='application/json')
 
 @app.route('/private/mystall', methods=['POST'])
 def mystall():
+    print(request.get_json(),flush=True)
     return Response(request.get_json(), status=200, mimetype='application/json')
 
 @app.route('/')
