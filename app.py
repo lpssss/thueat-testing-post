d8f1547a69c37a2dce29fb0950d48b2c7b769a35
lpssss <lim55570@gmail.com>
2021-12-01 19:01:38 +0800

    Update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def mystall_dishes():
 @app.route('/private/mystall', methods=['POST'])
 def mystall():
     print(request.get_json(),flush=True)
-    return Response(request.get_json(), status=200, mimetype='application/json')
+    return Response(request.json, status=200, mimetype='application/json')
 
 @app.route('/')
 def index():
